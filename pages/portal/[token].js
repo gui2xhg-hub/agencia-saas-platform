47cf7085c8fe2083afed0fb8f7491b390b474bb9
gui2xhg-hub <gui.2.x.hg@gmail.com>
2026-09-19 02:12:53 -0300

    Update [token].js

--- pages/portal/[token].js
+++ pages/portal/[token].js
@@ -1,6 +1,1 @@
 import { supabase } from '../../lib/supabase';
-
-// APAGUE estas linhas do seu arquivo [token].js:
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
